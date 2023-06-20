a01a38595ef440cd9f143076798368ab4094d8c9
Marvin Henßler <marvinhenssler@aol.com>
2023-06-20 15:53:43 +0200

    add additional profiles

--- src/app/mock-profiles.ts
+++ src/app/mock-profiles.ts
@@ -1,8 +1,9 @@
-import { faGithub, faXing, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faXing, faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { Profile } from "./profile";
 
 export const PROFILES: Profile[] = [
     {icon: faGithub, link: 'https://github.com/Rexjurgoran', name: 'Rexjurgoran (Marvin)'},
+    {icon: faLinkedin, link: 'https://www.linkedin.com/in/marvin-hen%C3%9Fler-63a8091b0/', name: 'Marvin Henßler'},
     {icon: faXing, link: 'https://www.xing.com/profile/Marvin_Henssler/cv', name: 'Marvin Henßler'},
-    {icon: faLinkedin, link: 'https://www.linkedin.com/in/marvin-hen%C3%9Fler-63a8091b0/', name: 'Marvin Henßler'}
+    {icon: faInstagram, link: 'https://instagram.com/marvinhenssler?igshid=MzNINGNkZWQ4Mg==', name: 'marvinhenssler'}
 ];
