3efd1e93d866d8f6e7eee8e897d233f865832f31
Marvin <82607475+Rexjurgoran@users.noreply.github.com>
2023-06-20 15:05:33 +0200

    Update mock-profiles.ts

--- src/app/mock-profiles.ts
+++ src/app/mock-profiles.ts
@@ -1,6 +1,8 @@
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faXing, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { Profile } from "./profile";
 
 export const PROFILES: Profile[] = [
-    {icon: faGithub, link: 'https://github.com/Rexjurgoran', name: 'Rexjurgoran (Marvin)'}
+    {icon: faGithub, link: 'https://github.com/Rexjurgoran', name: 'Rexjurgoran (Marvin)'},
+    {icon: faXing, link: 'https://www.xing.com/profile/Marvin_Henssler/cv', name: 'Marvin Henßler'},
+    {icon: faLinkedin, link: 'https://www.linkedin.com/in/marvin-hen%C3%9Fler-63a8091b0/', name: 'Marvin Henßler'}
 ];
