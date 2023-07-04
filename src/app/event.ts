5dfafb7b30f58c9ad6aedd2c4dc00a3ef34eae6b
Marvin Henßler <marvinhenssler@aol.com>
2023-07-04 14:28:36 +0200

    Event types with distinct colours

--- src/app/event.ts
+++ src/app/event.ts
@@ -1,5 +1,11 @@
+export enum EventType {
+    Education = "education",
+    Skill = "skill",
+    Position = "position"
+}
 export interface Event{
     date: string;
     name: string;
-    description: string;
+    description: string,
+    type: EventType
 }
--- src/app/mock-events.ts
+++ src/app/mock-events.ts
@@ -1,7 +1,33 @@
-import { Event} from "./event";
+import { Event, EventType} from "./event";
 
-export const EVENTS: Event[] = [
-    {date: '2023',name: 'Master degree', description:'Achieved master degree in IT-Management'},
-    {date: '2020',name: 'Bachelor degree', description:'Achieved bachelor degree in Business Informatics'},
-    {date: '2017',name: 'Finished school', description:'Finished school with advanced classes in physics and math'}
-];
+export const EVENTS: Event[] = [{
+    date: '2023',
+    name: 'Master degree', 
+    description:'Achieved master degree in IT-Management', 
+    type: EventType.Education
+},{
+    date: '2020',
+    name: 'Bachelor degree', 
+    description:'Achieved bachelor degree in Business Informatics', 
+    type: EventType.Education
+},{
+    date: '2018',
+    name: 'ABAP',
+    description: 'Learned ABAP for development in SAP during work.',
+    type: EventType.Skill
+},{
+    date: '2017-10',
+    name: 'Dual Student @ HARTING Technology Group',
+    description: 'Started working as dual student Business Informatics within HARTING Technology Group. Mainly doing SAP development and PLM configuration.',
+    type: EventType.Position
+},{
+    date: '2017-06',
+    name: 'Finished school', 
+    description:'Finished school with advanced classes in physics and math', 
+    type: EventType.Education
+},{
+    date: '2013',
+    name: 'Java',
+    description: 'Learned Java in informatics class during later years of school',
+    type: EventType.Skill
+}];
