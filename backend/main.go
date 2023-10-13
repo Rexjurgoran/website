8a72662e4d4d8ce801145f2032f53e85b369cb0f
Marvin Henßler <Marvin.Henssler@HARTING.com>
2023-10-13 11:27:15 +0200

    Add additional events to go

--- backend/main.go
+++ backend/main.go
@@ -41,6 +41,26 @@ func getEvents(response http.ResponseWriter, request *http.Request) {
 
 func buildEvents() []Event {
 	return []Event{
-		{time.Date(2023, time.January, 0, 0, 0, 0, 0, time.Now().Location()), "Master degree", "Achieved master degree in IT-Management", Education},
+		{
+			time.Date(2023, time.June, 30, 16, 45, 0, 0, time.Now().Location()),
+			"Master degree",
+			"Achieved master degree in IT-Management",
+			Education,
+		}, {
+			time.Date(2020, time.September, 18, 17, 45, 0, 0, time.Now().Location()),
+			"Bachelor degree",
+			"Achieved bachelor degree in Business Informatics",
+			Education,
+		}, {
+			time.Date(2017, time.August, 21, 8, 0, 0, 0, time.Now().Location()),
+			"Dual Student @ HARTING Technology Group",
+			"Started working as dual student Business Informatics within HARTING Technology Group. Mainly doing SAP development and PLM configuration.",
+			Position,
+		}, {
+			time.Date(2017, time.July, 7, 0, 0, 0, 0, time.Now().Location()),
+			"Finished school",
+			"Finished school with advanced classes in physics and math",
+			Education,
+		},
 	}
 }
