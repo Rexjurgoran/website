683fa56671dac69a4e5d05a5b8fc5ba111bcc576
Henßler <marvin.henssler@harting.com>
2024-05-16 14:45:52 +0200

    <#9>(feat): Move database coding to own file

--- backend/go.mod
+++ backend/go.mod
@@ -4,12 +4,13 @@ go 1.19
 
 require (
 	github.com/gorilla/mux v1.8.1
-	github.com/rs/zerolog v1.31.0
+	github.com/rs/zerolog v1.32.0
 	go.elastic.co/ecszerolog v0.2.0
+	go.mongodb.org/mongo-driver v1.15.0
 )
 
 require (
-	github.com/golang/snappy v0.0.1 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -19,7 +20,6 @@ require (
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	go.mongodb.org/mongo-driver v1.15.0 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"net/http"
 	"os"
@@ -10,8 +9,6 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/rs/zerolog/log"
 	"go.elastic.co/ecszerolog"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type EventType string
@@ -23,21 +20,14 @@ const (
 )
 
 type Event struct {
-	Date  time.Time `json:"date"`
-	Title string    `json:"title"`
-	Event string    `json:"event"`
-	Type  EventType `json:"type"`
+	Date  time.Time `json:"date" bson:"date"`
+	Title string    `json:"title" bson:"title"`
+	Event string    `json:"event" bson:"event"`
+	Type  EventType `json:"type" bson:"type"`
 }
-
-var ctx context.Context
-var client *mongo.Client
-var collection *mongo.Collection
 
 func main() {
 	createLogger()
-	createDBConnection()
-	createDBCollection()
-	fillDB()
 	router := mux.NewRouter()
 	router.HandleFunc("/events", getEvents).Methods("GET")
 	log.Fatal().Msg(http.ListenAndServe(":80", router).Error())
@@ -62,65 +52,13 @@ func createLogger() {
 	log.Logger = logger
 }
 
-func createDBConnection() {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	var err error
-	client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://"+os.Getenv("MONGODB_USERNAME")+":"+os.Getenv("MONGODB_PASSWORD")+"@mongodb:27017/"))
-	if err != nil {
-		log.Fatal().Msg(err.Error())
-	}
-}
-
-func createDBCollection() {
-	database := client.Database("database")
-	collection := database.Collection("collection")
-	collection.Drop(ctx)
-}
-
-func fillDB() {
-	events := buildEvents()
-	insertManyDocument := []interface{}{}
-	insertManyDocument = append(insertManyDocument, events)
-	_, err := collection.InsertMany(ctx, insertManyDocument)
-	if err != nil {
-		log.Error().Msg(err.Error())
-	}
-}
-
 func getEvents(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("Content-Type", "application/json")
-	events := buildEvents()
+	events := readEvents()
 	error := json.NewEncoder(response).Encode(events)
 	if error != nil {
 		log.Fatal().Msg(error.Error())
 	} else {
 		log.Info().Msg("getEvents() was successfull")
-	}
-}
-
-func buildEvents() []Event {
-	return []Event{
-		{
-			time.Date(2023, time.June, 30, 16, 45, 0, 0, time.Now().Location()),
-			"Master degree",
-			"Achieved master degree in IT-Management",
-			Education,
-		}, {
-			time.Date(2020, time.September, 18, 17, 45, 0, 0, time.Now().Location()),
-			"Bachelor degree",
-			"Achieved bachelor degree in Business Informatics",
-			Education,
-		}, {
-			time.Date(2017, time.August, 21, 8, 0, 0, 0, time.Now().Location()),
-			"Dual Student @ HARTING Technology Group",
-			"Started working as dual student Business Informatics within HARTING Technology Group. Mainly doing SAP development and PLM configuration.",
-			Position,
-		}, {
-			time.Date(2017, time.July, 7, 0, 0, 0, 0, time.Now().Location()),
-			"Finished school",
-			"Finished school with advanced classes in physics and math",
-			Education,
-		},
 	}
 }
